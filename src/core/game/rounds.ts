232896e18d828d48713d6371fb3f0afbed4a1b49
Mariia Kryvokhata <m.krivokhata@gmail.com>
2025-12-10 14:45:37 +0200

    feat: added diamonds to the rounds

--- src/core/game/rounds.ts
+++ src/core/game/rounds.ts
@@ -8,17 +8,22 @@ export function roundsFactory() {
     new GameRound(1, () => [
       new Comet(300, 400),
       new Planet(650, 400, 'red-giant'),
+      new Diamond(800, 350),
       new Earth(1000, 400)
     ]),
     new GameRound(2, () => [
       new Comet(200, 350),
       new Planet(550, 450, 'blackhole'),
+      new Diamond(700, 300),
+      new Diamond(800, 500),
       new Earth(900, 350)
     ]),
     new GameRound(3, () => [
       new Comet(400, 500),
       new Planet(700, 300, 'green-dwarf'),
       new Diamond(800, 400),
+      new Diamond(900, 350),
+      new Diamond(950, 500),
       new Planet(800, 500, 'red-giant'),
       new Earth(1100, 450)
     ]),
@@ -26,12 +31,19 @@ export function roundsFactory() {
       new Comet(250, 300),
       new Planet(600, 350, 'red-giant'),
       new Planet(750, 400, 'blackhole'),
+      new Diamond(700, 300),
+      new Diamond(850, 450),
+      new Diamond(900, 500),
       new Earth(950, 500)
     ]),
     new GameRound(5, () => [
       new Comet(350, 450),
       new Planet(500, 400, 'green-dwarf'),
       new Planet(650, 350, 'blackhole'),
+      new Diamond(600, 500),
+      new Diamond(800, 400),
+      new Diamond(950, 350),
+      new Diamond(1000, 450),
       new Earth(1050, 400)
     ])
   ];
